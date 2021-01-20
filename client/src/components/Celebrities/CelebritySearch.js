2af4bd7d172dfa643754854cfcb7f5edc8a36162
moabre <muhammadrehman21@gmail.com>
2021-01-20 03:32:21 -0600

    fixed errors causing issues with deployment

--- client/src/components/Celebrities/CelebritySearch.js
+++ client/src/components/Celebrities/CelebritySearch.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import '../../App.scss';
+import React from 'react'
+import '../../App.scss'
 const searchUrl = 'https://www.google.com/search?newwindow=1&tbm=isch&q='
 
 function toTitleCase(str) {
-  return str.replace(/[^-'\s]+/g, function(word) {
-      return word.replace(/^./, function(first) {
-      return first.toUpperCase();
-      });
-  });
+  return str.replace(/[^-'\s]+/g, function (word) {
+    return word.replace(/^./, function (first) {
+      return first.toUpperCase()
+    })
+  })
 }
 
-const CelebrityDiv = ({celeb}) => {
+const CelebrityDiv = ({ celeb }) => {
   return (
     <div className='celebrity-name'>
-      <a className='black' target="_blank" href={`${searchUrl}${celeb.name}`}>
+      <a
+        className='black'
+        target='_blank'
+        rel='noopener noreferrer'
+        href={`${searchUrl}${celeb.name}`}
+      >
         {`${toTitleCase(celeb.name)}`}
       </a>
-      {` ${(celeb.prob*100).toFixed(3)}%`}
+      {` ${(celeb.prob * 100).toFixed(3)}%`}
     </div>
-  );
-};
+  )
+}
 
-export default CelebrityDiv;
+export default CelebrityDiv
--- client/src/components/Face/Face.js
+++ client/src/components/Face/Face.js
@@ -6,7 +6,7 @@ const Face = ({ image, boxes }) => {
   return (
     <div className='center'>
       <div className='centerma'>
-        <img src={image} boxes={boxes} id='inputImage' />
+        <img src={image} boxes={boxes} id='inputImage' alt='persons img' />
         <BoxList boxes={boxes} />
       </div>
     </div>
